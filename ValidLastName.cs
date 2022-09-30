6dc090ac67f35876a50409c473983a6fa48edf81
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-30 14:10:26 +0530

    [Subodh]By Customexception check Last name

---
+++ ValidLastName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationEx
+{
+    public class ValidLastName
+    {
+        public static void ValidateLastName()
+        {
+            try
+            {
+                Console.WriteLine("Enter Last Name");
+                String lastName = Console.ReadLine();
+                string pattern = "^[A-Z]{1}[a-z]{2,}$";
+                if (Regex.IsMatch(lastName, pattern))
+                {
+                    Console.WriteLine("First Name : " + lastName);
+                }
+                else
+                {
+                    throw (new CustomException("Last name should be first letter capital and min 3 characters"));
+                }
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
